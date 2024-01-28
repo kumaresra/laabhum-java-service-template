07d279e001aa5e3327cce07369e8fbbdd4ad194f
Selvakumar Esra <selva@typesafe.me>
2024-01-28 21:27:39 +0800

    initial commit

---
+++ src/main/java/com/laabhum/posttradestreamingservice/PostTradeStreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.laabhum.posttradestreamingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostTradeStreamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostTradeStreamingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laabhum/posttradestreamingservice/PostTradeStreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laabhum.posttradestreamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostTradeStreamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
