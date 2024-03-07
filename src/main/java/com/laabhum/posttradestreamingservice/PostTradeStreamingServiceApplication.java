c323c3fd7737adb4e02b44ea1924287705c5e92f
Baskar Mohan <baskarmogn@outlook.com>
2024-03-07 22:20:16 +0530

    fx

--- src/main/java/com/laabhum/posttradestreamingservice/PostTradeStreamingServiceApplication.java
+++ src/main/java/com/laabhum/posttradestreamingservice/PostTradeStreamingServiceApplication.java
@@ -2,11 +2,7 @@ package com.laabhum.posttradestreamingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
+ 
 @SpringBootApplication
 public class PostTradeStreamingServiceApplication {
 
@@ -14,8 +10,5 @@ public class PostTradeStreamingServiceApplication {
 		SpringApplication.run(PostTradeStreamingServiceApplication.class, args);
 	}
 
-
-	
-	
-
+ 
 }
