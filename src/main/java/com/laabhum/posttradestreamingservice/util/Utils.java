14f048aee413b74b46077efdb02977d890325219
baskar <baskar@antfintech.in>
2024-03-09 23:35:52 +0800

    fx

--- src/main/java/com/laabhum/posttradestreamingservice/util/Utils.java
+++ src/main/java/com/laabhum/posttradestreamingservice/util/Utils.java
@@ -3,9 +3,12 @@ package com.laabhum.posttradestreamingservice.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laabhum.posttradestreamingservice.model.AggregationResult;
 import com.laabhum.posttradestreamingservice.model.SymbolDetail;
+import io.netty.util.internal.StringUtil;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class Utils {
@@ -25,6 +28,27 @@ public class Utils {
 	}
 
 	public static String generateSymbol(SymbolDetail symbol) {
-		return symbol.getExchange() + symbol.getName() + symbol.getExpiry() + symbol.getStrike() + symbol.getInstrumentType();
+		return symbol.getExchange() + symbol.getName() + convertExpiryString(symbol.getExpiry() )+ symbol.getStrike() + symbol.getInstrumentType();
 	}
+
+	
+	public static String convertExpiryString(String input) {
+
+
+		try{
+			// Parse input string as Instant
+			Instant instant = Instant.parse(input);
+
+			// Convert Instant to LocalDateTime
+			LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+
+			// Format LocalDateTime to desired string format
+			return dateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+			// Return the formatted date
+		}catch (Exception e){
+			return "";
+		}
+	}
+
 }
